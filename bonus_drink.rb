8898ea6bd311a70637e595f334abf6ed0e7c3f14
SakuraMymt <kyte.you.1@gmail.com>
2024-05-29 00:02:06 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,12 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    drink = amount
+    while(amount>=0)
+      bonus = amont/3
+      drink += bonus
+      amount = (amount/3)+(amount%3)
+    end
+    return drink
   end
 end
+BonusDrink.total_count_for(100)
